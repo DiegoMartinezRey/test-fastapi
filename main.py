44be6368f4d8fb101726358925fea047dd4f67b3
Diego Martinez <diegomrey1@gmail.com>
2025-05-22 23:20:46 +0200

    create get users and user by id

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    age: int
+
+
+users_list = [User(id=1, name="Brais", surname="Moure", age=35)]
+
+
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/users")
+async def users():
+    return users_list
+
+
+@app.get("/user/{id}")
+async def user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
